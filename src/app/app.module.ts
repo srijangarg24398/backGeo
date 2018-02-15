22c70f158b5068dbea20fbbc40140cfcaf019b33
Srijan <srijan@localhost.localdomain>
2018-02-15 13:11:52 +0530

    background geo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { BackgroundGeolocation } from '@ionic-native/background-geolocation';
 @NgModule({
   declarations: [
     MyApp,
@@ -24,7 +24,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    BackgroundGeolocation
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { BackgroundGeolocation, BackgroundGeolocationConfig, BackgroundGeolocationResponse } from '@ionic-native/background-geolocation';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  	config: BackgroundGeolocationConfig = {
+	    desiredAccuracy: 10,
+	    stationaryRadius: 20,
+	    distanceFilter: 30,
+	    debug: true,
+	    stopOnTerminate: false
+	};
+	location:BackgroundGeolocationResponse;
+  constructor(public navCtrl: NavController,private backgroundGeolocation: BackgroundGeolocation) {
+  	this.backgroundGeolocation.configure(this.config)
+  	.subscribe((location: BackgroundGeolocationResponse) => {
+    	console.log(location);
+    	this.location=location;
+    	this.backgroundGeolocation.finish();
+	});
+	this.backgroundGeolocation.start();
   }
 
 }
